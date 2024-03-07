b8af7c02dd6fcddc145b0dc39531e4e38404f165
Phuc Tran <tqp1610@gmail.com>
2024-03-07 21:50:35 +0700

    state for quiz

--- react-essentials/src/components/Quiz.tsx
+++ react-essentials/src/components/Quiz.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Quiz() {
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0); 
+  const [userAnswers, setUserAnswers] = useState([]); 
+
   return (
     <p>Currently active  Question</p>
   )
