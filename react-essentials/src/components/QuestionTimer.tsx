a596243eaab71752dc70ed131f93f7a69a400da6
Phuc Tran <tqp1610@gmail.com>
2024-03-08 16:24:14 +0700

    timeout

--- react-essentials/src/components/QuestionTimer.tsx
+++ react-essentials/src/components/QuestionTimer.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function QuestionTimer() {
-  return (
-    <div>QuestionTimer</div>
-  )
+export default function QuestionTimer({ timeout, onTimeout  }: any) {
+    const [remainingTime, setRemainingTime] = useState(timeout);
+
+    setTimeout(onTimeout(), timeout);
+
+    useEffect(() => {
+        
+    }, [])
+    
+
+    setInterval(() => {
+        setRemainingTime((prev: number) => prev - 100)
+    }, 100)
+
+    return (
+        <progress id='question-time ' />
+    )
 }
