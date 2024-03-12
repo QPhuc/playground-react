f66a49cb854f3f5adc7552c8738d174894d95511
Phuc Tran <tqp1610@gmail.com>
2024-03-12 21:37:56 +0700

    update props for Answer

--- react-essentials/src/components/Answers.tsx
+++ react-essentials/src/components/Answers.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Answers() {
+export default function Answers({ansers, selectedAnser, answerState}: any) {
   return (
       <ul id='answers'>
           {shuffledAnswers.current.map((answer, index) => {
